5fa724f59780b794db4b363fd8f96d4f0f4c8798
r7zex <151392460+r7zex@users.noreply.github.com>
2026-05-11 20:34:07 -0400

    feat: migrate and expand Gradient Descent topic to new curriculum format

--- src/data/aiCurriculum.ts
+++ src/data/aiCurriculum.ts
@@ -2,6 +2,7 @@ import type { FlowTopic } from './aiCurriculumTypes'
 import { curriculumBlocks } from './curriculum/helpers'
 import { introTopics } from './curriculum/intro'
 import { topicMetricsPrf } from './curriculum/metrics'
+import { topicGradientDescent } from './curriculum/optimization'
 import { numpyTopics } from './curriculum/numpy/index'
 
 export { curriculumBlocks }
@@ -9,5 +10,6 @@ export { curriculumBlocks }
 export const flowTopics: FlowTopic[] = [
   ...introTopics,
   topicMetricsPrf,
+  topicGradientDescent,
   ...numpyTopics,
 ]
