836a5df12a093bb8e7d2cbd8f970e261f469dc4c
r7zex <151392460+r7zex@users.noreply.github.com>
2026-05-11 20:47:39 -0400

    feat: add advanced theory (Bias-Variance Tradeoff) to Module 1

--- src/data/aiCurriculum.ts
+++ src/data/aiCurriculum.ts
@@ -2,6 +2,7 @@ import type { FlowTopic } from './aiCurriculumTypes'
 import { curriculumBlocks } from './curriculum/helpers'
 import { topicAiMlDlV2, topicMlTypes } from './curriculum/intro_v2'
 import { topicMlProjectLifecycle, topicMetricsDeep } from './curriculum/intro_v2_part2'
+import { topicBiasVariance } from './curriculum/intro_v2_advanced'
 import { topicMetricsPrf } from './curriculum/metrics'
 import { topicGradientDescent } from './curriculum/optimization'
 import { topicGiniImpurity } from './curriculum/trees'
@@ -15,6 +16,7 @@ export const flowTopics: FlowTopic[] = [
   topicMlTypes,
   topicMlProjectLifecycle,
   topicMetricsDeep,
+  topicBiasVariance,
   topicGradientDescent,
   topicGiniImpurity,
   ...numpyTopics,
